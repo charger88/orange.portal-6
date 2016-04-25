4c834b0ad32d01a4a522b37551fa2f029bd424d7
Mikhail Kelner <mikhail.kelner@gmail.com>
2016-04-25 12:55:13 +0300

    Feedback module

---
+++ modules/feedback/lang/en.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang = array(
+
+    'MODULE_FEEDBACK' => 'Feedback',
+
+    'MODULE_FEEDBACK_NO_FORM' => 'Feedback form not found',
+
+    'MODULE_FEEDBACK_THEME' => 'Theme',
+    'MODULE_FEEDBACK_PHONE' => 'Phone',
+    'MODULE_FEEDBACK_EMAIL' => 'Email',
+    'MODULE_FEEDBACK_UNAME' => 'Name',
+    'MODULE_FEEDBACK_TEXT'  => 'Message',
+
+    'MODULE_FEEDBACK_SEND'  => 'Send',
+
+    'MODULE_FEEDBACK_MESSAGE_SENT' => 'Message was sent',
+    'MODULE_FEEDBACK_SPAM_DETECTED' => 'Sorry, our spam-detector reports that something goes wrong. Please, try again.',
+
+);
