fc4770aff0359c9ca69c86b0d59a8ff14d27eaac
Mikhail Kelner <mikhail.kelner@gmail.com>
2016-10-02 12:49:43 +0300

    Switch Filters functionality to library

--- core/model/content_text.class.php
+++ core/model/content_text.class.php
@@ -5,6 +5,12 @@
  */
 class OPAM_Content_Text extends \Orange\Database\ActiveRecord {
 
+    const FORMAT_AS_IS = 0;
+    const FORMAT_AUTOFORMAT = 1;
+    const FORMAT_PLAIN_TEXT = 2;
+    const FORMAT_SAFE_HTML = 3;
+    const FORMAT_COMMENTS_MODE = 4;
+
     /**
      * @var string
      */
@@ -32,11 +38,31 @@ class OPAM_Content_Text extends \Orange\Database\ActiveRecord {
     protected static $uniq = array(array('content_id','content_text_role'));
 
     public function format(){
-        $replaces = array(
+        switch ($this->get('content_text_format')){
+            case self::FORMAT_AS_IS:
+                $text = $this->get('content_text_value');
+                break;
+            case self::FORMAT_AUTOFORMAT:
+                $text = nl2br($this->get('content_text_value'));
+                break;
+            case self::FORMAT_PLAIN_TEXT:
+                $text = \Orange\Filters\SimpleFilters::escAsText($this->get('content_text_value'));
+                break;
+            case self::FORMAT_SAFE_HTML:
+                $text = (new \Orange\Filters\HtmlFilter())->parse($this->get('content_text_value'));
+                break;
+            case self::FORMAT_COMMENTS_MODE:
+                $text = \Orange\Filters\SimpleFilters::esc($this->get('content_text_value'));
+                $text = \Orange\Filters\SimpleFilters::escAsTextWithQuotes($text);
+                break;
+            default:
+                throw new \Exception('Unknown format type: ' . $this->get('content_text_format'));
+                break;
+        }
+        $replaces = [
             '%%url%%'      => OP_WWW,
             '%%sitename%%' => OPAL_Portal::config('system_sitename'),
-        );
-        $text = OPAL_Parser::formatText($this->get('content_text_value'),$this->get('content_text_format'));
+        ];
         foreach ($replaces as $code => $value){
             $text = str_replace($code, $value, $text);
         }
