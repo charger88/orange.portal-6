6fed22954ef3e86d5c2f29515a9029dd23ece0f2
Mikhail Kelner <mikhail.kelner@gmail.com>
2016-10-27 00:00:10 +0300

    Search form fix

--- modules/system/controllers/forms/search.class.php
+++ modules/system/controllers/forms/search.class.php
@@ -7,7 +7,7 @@ use \Orange\Forms\Fields\Buttons\Submit;
 class OPMF_System_Search extends Form {
 		
 	protected function init($params){
-        $this->addField((new Search('search', OPAL_Lang::t('SEARCH')))->placeholder());
+        $this->addField((new Search('search', OPAL_Lang::t('SEARCH')))->placeholder()->requireField());
         $this->addField((new Submit('signin_submit', OPAL_Lang::t('FIND'))));
     }
 	
